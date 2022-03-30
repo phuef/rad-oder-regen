375ad5f46b02b8f0b700e011711ed1f069e2367f
phuef <phil.hueffer@gmail.com>
2022-03-30 17:06:27 +0200

    initial commit

---
+++ main/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    test
+    <v-card>
+      
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
